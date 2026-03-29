f97c08686cc2f96ccdc1312b7371ce9378464b30
Asus <vovan-gr@mail.ru>
2026-03-29 11:42:01 +0300

    Добавил 3 теста в LoginWithWrongCredentials

--- module_1/src/main/java/core/base/BasePage.java
+++ module_1/src/main/java/core/base/BasePage.java
@@ -1,32 +1,47 @@
 package core.base;
 
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$x;
 
-public abstract class BasePage {
+public class BasePage {
 
-    private String cssSelector;
-    // Примеры общих элементов, которые могут использоваться на разных страницах
-    protected SelenideElement headerLogo = $("[tsid='toolbar_logo']");
-    protected SelenideElement searchField = $x("//input[@name='st.query']");
-    protected SelenideElement vkServices = $("[data-l='t,vk_ecosystem']");
+    protected SelenideElement headerLogo = $(" [tsid='toolbar_logo'] ");
+    protected SelenideElement searchField = $(" [name='st.query'] ");
+    protected SelenideElement vkServices = $(" [data-l='t,vk_ecosystem'] ");
+    protected SelenideElement acceptCookieButton = $(" #cb_accept ");
+    protected SelenideElement acceptPrivacyButton = $(" .cmpbox_btn_yes ");
 
-    // Метод для поиска по сайту
+    @Step("Выполняем поиск по сайту с запросом: {query}")
     public void search(String query) {
         searchField.shouldBe(visible).setValue(query).pressEnter();
     }
 
-    // Пример общего метода для клика по иконке уведомлений
+    @Step("Открываем VK Services")
     public void openVkServices() {
         vkServices.shouldBe(visible).click();
     }
+
+    @Step("Кликаем на логотип ОК")
     public void clickLogo() {
         headerLogo.shouldBe(visible).click();
     }
 
+    @Step("Принимаем куки")
+    public void acceptCookie() {
+        if (acceptCookieButton.exists()) {
+            acceptCookieButton.click();
+        }
+    }
 
+    @Step("Принимаем политику конфиденциальности")
+    public void acceptPrivacyButton() {
+        if (acceptPrivacyButton.exists()) {
+            acceptPrivacyButton.click();
+        }
+    }
 }
+
 
---
+++ module_1/src/test/java/tests/LoginWithWrongCredentials.java
@@ -0,0 +1,67 @@
+package tests;
+
+import core.base.BaseTest;
+import core.pages.LoginPage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LoginWithWrongCredentials extends BaseTest {
+
+    private static LoginPage loginPage;
+
+    @BeforeEach
+    public void prepare() {
+        open(baseUrl);
+        // Принятие cookies и политики
+        loginPage = new LoginPage();
+        loginPage.acceptCookie();
+        loginPage.acceptPrivacyButton();
+    }
+
+    @Test
+    public void loginTest() {
+        // Попытка входа с некорректными данными
+        loginPage.login("incorrectUser", "incorrectPassword");
+
+        // Проверка наличия сообщения об ошибке
+        assertTrue(loginPage.isErrorMessageVisible(), "Сообщение об ошибке входа не отображается");
+
+        //Проверка текста сообщения об ошибке
+        String expectedErrorMessage = "Неправильно указан логин и/или пароль";
+        String actualErrorMessage = loginPage.getErrorMessageText();
+        assertEquals(expectedErrorMessage, actualErrorMessage, "Текст сообщения об ошибке не совпадает");
+    }
+
+    @Test
+    public void emptyLoginTest() {
+        // Попытка входа с некорректными данными
+        loginPage.login("", "incorrectPassword");
+
+        // Проверка наличия сообщения об ошибке
+        assertTrue(loginPage.isErrorMessageVisible(), "Сообщение об ошибке входа не отображается");
+
+        //Проверка текста сообщения об ошибке
+        String expectedErrorMessage = "Введите логин";
+        String actualErrorMessage = loginPage.getErrorMessageText();
+        assertEquals(expectedErrorMessage, actualErrorMessage, "Текст сообщения об ошибке не совпадает");
+    }
+
+    @Test
+    public void emptyPasswordTest() {
+        // Попытка входа с некорректными данными
+        loginPage.login("incorrectUser", "");
+
+        // Проверка наличия сообщения об ошибке
+        assertTrue(loginPage.isErrorMessageVisible(), "Сообщение об ошибке входа не отображается");
+
+        //Проверка текста сообщения об ошибке
+        String expectedErrorMessage = "Введите пароль";
+        String actualErrorMessage = loginPage.getErrorMessageText();
+        assertEquals(expectedErrorMessage, actualErrorMessage, "Текст сообщения об ошибке не совпадает");
+    }
+}
